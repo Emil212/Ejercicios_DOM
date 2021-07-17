d1f26eee4274049a5d5fcb42d7739bbcbbbd7369
Emiliano Ceroncd Waifu-Administratorgit config --global user.name Emiliano <hceronj1400@alumno.ipn.mx>
2021-07-17 07:31:58 -0500

    Deteccion camara  web

--- JS/index_dom.js
+++ JS/index_dom.js
@@ -8,6 +8,7 @@ import responsiveMedia from "./objeto_responsive.js";
 import responsiveTester from "./prueba_responsive.js";
 import userDeviceInfo from "./deteccion_dispositivos.js";
 import networkStatus from "./deteccion_red.js";
+import webCam from "./deteccion_webcam.js";
 
 const d = document;
 
@@ -37,6 +38,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
 
   responsiveTester("responsive-tester");
   userDeviceInfo("user-device");
+  webCam("webcam");
 });
 
 //No se incluye en el DOMContentLoaded deido a que no muestra nada
